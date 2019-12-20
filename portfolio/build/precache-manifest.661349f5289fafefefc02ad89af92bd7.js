038c50cef56278a83f1b11da31b3dd6b2e199144
Shriram <shriram.r@nutanix.com>
2019-12-20 17:30:41 +0530

    build portfolio v0.1

---
+++ portfolio/build/precache-manifest.661349f5289fafefefc02ad89af92bd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c06a641a7bc8477b322aa6d6107bbc5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c7afe59e734a4ceddec",
+    "url": "/static/css/2.bea6f0b1.chunk.css"
+  },
+  {
+    "revision": "e427f0d739cce0bf21f3",
+    "url": "/static/css/main.54c18f54.chunk.css"
+  },
+  {
+    "revision": "7c7afe59e734a4ceddec",
+    "url": "/static/js/2.7259d6d9.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.7259d6d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e427f0d739cce0bf21f3",
+    "url": "/static/js/main.5de1e1e4.chunk.js"
+  },
+  {
+    "revision": "058eec7f45d9072bee76",
+    "url": "/static/js/runtime-main.c1f4bcb5.js"
+  }
+]);
