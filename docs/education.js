8eb3b3d0c904b048b534971a10bfe5e351e322bb
Shriram <shriram.r@nutanix.com>
2019-12-20 14:22:17 +0530

    Portfolio init

---
+++ docs/education.js
@@ -0,0 +1,9 @@
+// DO NOT CHANGE THE VARIABLE NAME
+var educationData = [{
+  "year": "2015",
+  "header": "Bits Pilani", // Institute Name
+  "content": "B.Hons in Computer Science", // Add some description here
+  "achievements": [
+    "" // Add few achievement here, you can leave the list empty/null too
+  ]
+}]
